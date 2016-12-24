9c3b03295e31fcbcbc773366a40ed8bfbbac9abc
Craexy <christopher.utsch@gmail.com>
2016-12-24 12:14:03 +0100

    Verbindung zwischen Aktivitäten

--- app/src/main/java/com/dev/app/mensabuddy/StartActivity.java
+++ app/src/main/java/com/dev/app/mensabuddy/StartActivity.java
@@ -1,12 +1,14 @@
 package com.dev.app.mensabuddy;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static com.dev.app.mensabuddy.R.id.button;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
@@ -32,5 +36,15 @@ public class StartActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        Button button = (Button) findViewById(R.id.matchingBtn);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), StartMatchingActivity.class);
+                startActivity(i);
+            }
+
+        });
     }
 }
